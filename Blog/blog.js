647b92f5a8a8eb64e4ce7527711377e275a769bd
Rakibul Hasan <rakibulhasanbu@gmail.com>
2023-02-18 10:09:11 +0600

    interacting blog page and home page

---
+++ Blog/blog.js
@@ -0,0 +1,4 @@
+// add links geometry blog page to geometry home page
+document.querySelector("#btn-home").addEventListener("click", function () {
+  location.href = "../index.html";
+});
---
+++ js/geomentry.js
@@ -0,0 +1,4 @@
+// add links to geometry blog page
+document.querySelector("#btn-blog").addEventListener("click", function () {
+  location.href = "Blog/blog.html";
+});
